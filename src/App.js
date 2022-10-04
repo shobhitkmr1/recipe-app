146db02f81b15706a133e43ff8b33bf58595f5b1
Shobhitkmr <43270097+shobhitkmr1@users.noreply.github.com>
2022-10-04 15:12:36 +0530

    1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Recipe from './Recipe';
 import './App.css';
 
 function App() {
+  const APP_ID = "e67ae94d";
+  const APP_KEY = "a51323e563d97be454099eb933987445";
+
+  const [recipes,setRecipes]=useState([]);
+  const [search,setSearch]=useState("");
+  const [query,setQuery]=useState('chicken');
+
+  useEffect(() => {
+    getRecipes();
+  },[query]);
+  const getRecipes = async()=>{
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className='search-form'>
+        <input onChange={updateSearch} className='search-bar' type="text" value={search}/>
+        <button className='search-button' type="submit">Search</button>
+      </form>
+      <div className='recipes'>
+      {recipes.map(recipe=>(
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        />
+      ))}
+      </div>
     </div>
   );
 }
